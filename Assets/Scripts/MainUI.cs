ceae3e7d3e0d013b87183a14b3b4d40867660611
Christopher Aytona <christopher.aytona@gmail.com>
2017-01-30 20:12:43 -0500

    Changed cursor mat and added scrolling bg

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUI : MonoBehaviour {
+
+    [SerializeField]
+    private List<Canvas> m_Canvases;
+
+    public void SwitchScenes(Scene _target) {
+        SceneManager.LoadScene(_target.buildIndex);
+    }
+
+    public void SwitchCanvas(Canvas _target) {
+        foreach (Canvas i in m_Canvases) {
+            if (i.enabled) {
+                i.enabled = false;
+                break;
+            }
+        }
+        _target.enabled = true;
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Materials {
+    public class ScrollingTexture : MonoBehaviour {
+        public Material m_Texture;
+        public Vector2 m_Direction;
+
+        private Renderer m_Renderer;
+
+        // TODO: Make a class that does this so other scripts can use it in the future
+        private static ScrollingTexture m_Instance;
+
+        void Awake() {
+            if (m_Instance == null) {
+                m_Instance = FindObjectOfType<ScrollingTexture>();
+            } else {
+                Destroy(this);
+            }
+        }
+        // Note: How does the user know which script will get deleted (From testing it seems like it goes bottom->up from the hierarchy)
+        // End of TODO
+
+        void Start() {
+            m_Renderer = GetComponent<Renderer>();
+        }
+
+        void Update() {
+            OffsetTexture();
+        }
+
+        private void OffsetTexture() {
+            m_Texture.mainTextureOffset += m_Direction / 100f; // Incase they put in 1 which will look like its not moving
+            // Just resets it back to 0 so the material won't get absurbly large numbers in the offset
+            if (m_Texture.mainTextureOffset.x >= 1) {
+                m_Texture.mainTextureOffset = new Vector2(0, m_Texture.mainTextureOffset.y);
+            }
+            if (m_Texture.mainTextureOffset.y >= 1) {
+                m_Texture.mainTextureOffset = new Vector2(m_Texture.mainTextureOffset.x, 0);
+            }
+        }
+    }
+}
